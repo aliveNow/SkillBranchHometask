8f3f4a640e988b22dd886669854f9a203e27519f
Самсонова Мария Андреевна <samsonova.mariya2@wildberries.ru>
2020-07-26 20:51:49 +0300

    - User class from video lesson

--- app/src/main/java/ru/skillbranch/kotlinexample/User.kt
+++ app/src/main/java/ru/skillbranch/kotlinexample/User.kt
@@ -1,20 +1,102 @@
 package ru.skillbranch.kotlinexample
 
+import androidx.annotation.VisibleForTesting
+import java.lang.StringBuilder
+import java.math.BigInteger
+import java.security.MessageDigest
+import java.security.SecureRandom
+
 class User private constructor(
-    val firstName: String,
-    val lastName: String?,
+    private val firstName: String,
+    private val lastName: String?,
     email: String? = null,
     rawPhone: String? = null,
     meta: Map<String, Any>? = null
 ) {
 
     val userInfo: String
-    val fullName: String
-    val initials: String
-    var phone: String?
-    var login: String
-    val salt: String
-    var passwordHash: String
-    var accessCode: String? //@VisibleForTesting(otherwise = VisibleForTesting.NONE)
+    private val fullName: String
+        get() = listOfNotNull(firstName, lastName)
+            .joinToString(" ")
+            .capitalize()
+    private val initials: String
+        get() = listOfNotNull(firstName, lastName)
+            .mapNotNull { it.takeIf { it.isNotEmpty() }?.first() }
+            .joinToString { " " }
+            .toUpperCase()
+    private var phone: String? = null
+        set(value) {
+            field = value?.replace("[^+\\d]".toRegex(), "")
+        }
+    private var _login: String? = null
+    private var login: String
+        get() = _login!!
+        set(value) {
+            _login = value.toLowerCase()
+        }
+    private val salt: String by lazy {
+        ByteArray(16).also { SecureRandom().nextBytes(it) }.toString()
+    }
+    private lateinit var passwordHash: String
+
+    @VisibleForTesting(otherwise = VisibleForTesting.NONE)
+    var accessCode: String? = null
+
+    constructor(
+        firstName: String,
+        lastName: String?,
+        email: String,
+        password: String
+    ) : this(firstName, lastName, email = email, meta = mapOf("auth" to "sms")) {
+        passwordHash = encrypt(password)
+    }
+
+    constructor(
+        firstName: String,
+        lastName: String?,
+        rawPhone: String
+    ) : this(firstName, lastName, rawPhone = rawPhone, meta = mapOf("auth" to "password")) {
+        val code = generateAccessCode()
+        passwordHash = encrypt(code)
+        accessCode = code
+        sendAccessCodeToUser(phone!!, code)
+    }
+
+    init {
+        userInfo = """
+      firstName: $firstName
+      lastName: $lastName
+      login: $login
+      fullName: $fullName
+      initials: $initials
+      email: $email
+      phone: $phone
+      meta: $meta
+    """.trimIndent()
+    }
+
+    private fun encrypt(password: String): String = salt.plus(password).md5()
+
+    private fun generateAccessCode(): String {
+        val possible = "ABCDEFGHIJKLNOPQRSTUVWXYZabcdefghijklnopqrstuvwxyz0123456789"
+        return StringBuilder().apply {
+            repeat(6) {
+                possible.indices.random().also { index ->
+                    append(possible[index])
+                }
+            }
+        }.toString()
+    }
+
+    private fun sendAccessCodeToUser(phone: String, code: String) {
+        println("....... sending access code: $code on $phone")
+    }
+
+    private fun String.md5(): String {
+        val md = MessageDigest.getInstance("MD5")
+        val digest = md.digest(toByteArray()) //16 byte
+        val hexString = BigInteger(1, digest).toString(16)
+        return hexString.padStart(32, '0')
+    }
 
 }
